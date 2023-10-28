62c14a2e474ef6ebb951b3051312a2740f8847e2
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-10-28 19:32:22 +0100

    add mode selection logic starting to twoscreen files

---
+++ game.js
@@ -0,0 +1,21 @@
+
+
+// Player Bot Screen Controller
+function PlayerBotScreenController() {
+    const playerBotModeBtn = document.querySelector("button.bot-vs-player-mode");
+
+    console.log(playerBotModeBtn);
+}
+PlayerBotScreenController();
+
+function PlayerPlayerScreenController() {
+    const playerPlayerModeBtn = document.querySelector("button.player-vs-player-mode");
+
+    console.log(playerPlayerModeBtn);
+}
+PlayerPlayerScreenController();
+
+// Mode Switching Module
+function ModeSwitching() {
+
+}
